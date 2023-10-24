42fbae8f98bf769e4365193945a592348691e822
Mobinshahidi <mowbinsh@gmail.com>
2023-10-24 22:05:10 +0330

    some edit on main

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import VoiceAssistant from './component/VoiceAssistant';
 ReactDOM.createRoot(document.getElementById('root')).render(
-	// <React.StrictMode>
 		<VoiceAssistant />
-	// </React.StrictMode>,
 );
